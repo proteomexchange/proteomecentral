2325fde53b0afbc189bffb9ba07f34ae68049c7b
Eric Deutsch <edeutsch@systemsbiology.org>
2012-12-06 22:34:24 +0000

    Missed file
    git-svn-id: https://proteomexchange.googlecode.com/svn/proteomecentral@8 b7631ac0-c227-11dd-874b-8bacd04a0a74

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<?php
+  $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+  /* $Id$ */
+  $TITLE="TEMPLATETITLE";
+  $SUBTITLE="TEMPLATESUBTITLE";
+  include("$DOCUMENT_ROOT/includes/header.inc.php");
+?>
+
+<!-- BEGIN main content -->
+
+<!-- END main content -->
+
+<?php
+  include("$DOCUMENT_ROOT/includes/footer.inc.php");
+?>
+</html>
