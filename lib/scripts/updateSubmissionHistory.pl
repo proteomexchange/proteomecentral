111a411c8827d678d60d0df5b4371c54898fbd3f
Eric Deutsch <edeutsch@systemsbiology.org>
2013-10-01 17:28:11 +0000

    Major finishing work so that it now properly populates the history table
    git-svn-id: https://proteomexchange.googlecode.com/svn/proteomecentral@37 b7631ac0-c227-11dd-874b-8bacd04a0a74

--- lib/scripts/updateSubmissionHistory.pl
+++ lib/scripts/updateSubmissionHistory.pl
@@ -9,6 +9,7 @@ use lib "$FindBin::Bin/../perl";
 use ProteomeXchange::Database;
 use ProteomeXchange::DatasetParser;
 
+my $db;
 my $command = shift;
 
 main();
@@ -25,7 +26,7 @@ sub main {
     return(0);
   }
 
-  my $db = new ProteomeXchange::Database;
+  $db = new ProteomeXchange::Database;
   die("ERROR: Unable to connect to database") unless ($db->getStatus eq 'connected');
 
   if (uc($command) eq 'TEST') {
@@ -34,7 +35,7 @@ sub main {
     return(1);
 
   } elsif (uc($command) eq 'UPDATE') {
-    print "This has not been implemented yet\n";
+    updateSubmissionHistory(test=>0);
     return(1);
 
   } else {
@@ -56,6 +57,7 @@ sub updateSubmissionHistory {
 
   #### Decode the argument list
   my $test = $args{'test'} || 0;
+  my $tableName = 'datasetHistory';
 
   my $submissionsDirectory = "/net/dblocal/wwwspecial/proteomecentral/var/submissions";
 
@@ -83,7 +85,7 @@ sub updateSubmissionHistory {
       my $identifier = $response->{dataset}->{identifier};
       if ($identifier =~ /PDX/) {
 	print "  ERROR: Correcting $identifier to PXD (i.e. PDX -> PXD).\n";
-	$identifier =~ s/PXD/PDX/;
+	$identifier =~ s/PDX/PXD/;
       }
       print "  identifier = $identifier  timestamp = $timestamp\n";
       $submissions{byIdentifier}->{$identifier}->{byTimestamp}->{$timestamp} = $response;
@@ -95,6 +97,8 @@ sub updateSubmissionHistory {
   }
 
   #### Now summarize by dataset
+  print "=============================\n";
+  print "==== Summary by dataset =====\n";
   print "=============================\n";
   my @identifiers = sort(keys(%{$submissions{byIdentifier}}));
   foreach my $identifier ( @identifiers ) {
@@ -108,6 +112,141 @@ sub updateSubmissionHistory {
       print "  $timestamp  $tmp\n";
     }
   }
+
+
+  #### Get all current data from the main table in order to write it to the history table
+  print "================================================\n";
+  print "==== Get and iterate through history table =====\n";
+  print "================================================\n";
+
+  my $sql = "SELECT dataset_id,datasetIdentifier,identifierVersion,isLatestVersion,PXPartner,status,primarySubmitter,title,species,instrument,publication,keywordList,announcementXML,identifierDate,submissionDate,revisionDate FROM dataset";
+  my @rows = $db->selectSeveralColumns($sql);
+
+  my $nRows = scalar(@rows);
+  if (@rows) {
+    print "Successfully selected $nRows rows\n";
+  } else {
+    print "ERROR: Failed to get rows from dataset table\n";
+    return;
+  }
+
+  #### FIXME FIXME  ADD labHead !!!!!!!!!!!!!!!!!!!!1
+
+  foreach my $row ( @rows ) {
+    my ( $dataset_id,$datasetIdentifier,$identifierVersion,$isLatestVersion,$PXPartner,$status,$primarySubmitter,$title,$species,$instrument,$publication,$keywordList,$announcementXML,$identifierDate,$submissionDate,$revisionDate ) = @{$row};
+
+    print "** dataset_id = $dataset_id    datasetIdentifier = $datasetIdentifier\n";
+    print "   identifierVersion = $identifierVersion   isLatestVersion = $isLatestVersion\n";
+    print "   status = $status\n";
+    print "   title = $title\n" if ($title);
+    print "   identifierDate = $identifierDate\n" if ($identifierDate);
+    print "   submissionDate = $submissionDate\n" if ($submissionDate);
+    print "   revisionDate = $revisionDate\n" if ($revisionDate);
+
+    my $iVersion = 0;
+    my $originalSubmissionDate = 'NULL';
+
+    #### Set the row values
+    my %rowdata = (
+      dataset_id => $dataset_id,
+      datasetIdentifier => $datasetIdentifier,
+      identifierVersion => $iVersion,
+      isLatestVersion => 'N',
+      PXPartner => $PXPartner,
+      status => 'ID requested',
+      identifierDate => $identifierDate,
+    );
+
+    #### Insert the new record
+    my $datasetHistory_id = $db->updateOrInsertRow(
+      insert => 1,
+      table_name => $tableName,
+      rowdata_ref => \%rowdata,
+      return_PK => 1,
+      testonly => 0,
+      verbose => 1,
+    );
+
+    $iVersion++;
+
+    #### Iterate over all file submissions for this dataset
+    my @timestamps = sort(keys(%{$submissions{byIdentifier}->{$datasetIdentifier}->{byTimestamp}}));
+    foreach my $timestamp ( @timestamps ) {
+      my $dataset = $submissions{byIdentifier}->{$datasetIdentifier}->{byTimestamp}->{$timestamp}->{dataset};
+      my $description = $dataset->{description} || '???';
+
+      my $changeLogEntry = $dataset->{changeLogEntry} || '';
+      $changeLogEntry = substr($changeLogEntry,0,255);
+
+      my $timestampStr = $timestamp;
+      $timestampStr =~ s/\_/ /;
+
+      $originalSubmissionDate = $timestampStr if ($iVersion == 1);
+
+      $revisionDate = 'NULL';
+      $revisionDate = $timestampStr if ($iVersion > 1);
+
+      $publication = $dataset->{publication} || '???';
+
+      unless ($changeLogEntry) {
+	$changeLogEntry = 'NULL' if ($iVersion < 2);
+	$changeLogEntry = 'missing' if ($iVersion > 1);
+      }
+
+      #### Set the row values
+      %rowdata = (
+        dataset_id => $dataset_id,
+        datasetIdentifier => $datasetIdentifier,
+        identifierVersion => $iVersion,
+        isLatestVersion => 'N',
+        PXPartner => $PXPartner,
+        status => 'announced',
+        primarySubmitter => $primarySubmitter,
+        labHead => 'NULL',
+        title => $title,
+        species => $species,
+        instrument => $instrument,
+        publication => $publication,
+        keywordList => $keywordList,
+        announcementXML => $announcementXML,
+        identifierDate => $identifierDate,
+        submissionDate => $originalSubmissionDate,
+        revisionDate => $revisionDate,
+        changeLogEntry => $changeLogEntry,
+      );
+
+      #### Insert the new record
+      $datasetHistory_id = $db->updateOrInsertRow(
+        insert => 1,
+        table_name => $tableName,
+        rowdata_ref => \%rowdata,
+        return_PK => 1,
+        testonly => 0,
+        verbose => 1,
+      );
+
+      $iVersion++;
+
+    }
+
+    #### Set the row values
+    %rowdata = (
+      isLatestVersion => 'Y',
+    );
+
+    #### Update the last record with being the latest version
+    my $result = $db->updateOrInsertRow(
+      update => 1,
+      PK => 'datasetHistory_id',
+      PK_value => $datasetHistory_id,
+      table_name => $tableName,
+      rowdata_ref => \%rowdata,
+      testonly => 0,
+      verbose => 1,
+    );
+
+  }
+
 
   return;
 
