fd796632a6b85deb851f42bb99ea730e83faff45
Eric Deutsch <edeutsch@systemsbiology.org>
2015-07-14 18:36:03 +0000

    Added a contact us link
    git-svn-id: https://proteomexchange.googlecode.com/svn/proteomecentral@269 b7631ac0-c227-11dd-874b-8bacd04a0a74

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,9 +5,12 @@
 
 	<table align="center" bgcolor="#f4f6f8">
 	<tr><td>
+	If you have a question or comment about ProteomeXchange, please <a href="http://www.proteomexchange.org/contact">contact us</a>!
+	</td></tr>
+	<tr><td>
 	<img src="/images/ebi_footer_logo.png">&nbsp;&nbsp;&nbsp;&nbsp;<img src="/images/isb.png">&nbsp;&nbsp;&nbsp;&nbsp;<img src="/images/capacities.png">
 	</td></tr>
-	
+
 	</table>			
 	<table width="100%"><tr><td bgcolor="#c6dee7" height="15"></td></tr></table>
 	<table height="100%" width="100%" bgcolor="#000000"><tr><td></td></tr></table>	
