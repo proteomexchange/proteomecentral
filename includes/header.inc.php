1d4274cc06bd23e2334734725462a44d567be9c3
Luis Mendoza <lmendoza@mimas.systemsbiology.net>
2019-10-18 09:51:48 -0700

    Add pretty shadow to header; remove -x property from footer file

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -7,7 +7,7 @@
 </head>
 
 <body bgcolor="#f4f6f8" marginwidth="0" marginheight="0">
-	<table width="100%" height="127" background="/images/topbackground.jpg">
+	<table width="100%" height="127" background="/images/topbackground.jpg" style="box-shadow: 0 6px 8px 0 rgba(0,0,0,0.3);">
 	<tr align="center"><td><a href="http://www.proteomexchange.org/" border="0"><img src="/images/proteomexchange.png"></a></td><td><a href="http://proteomecentral.proteomexchange.org/" border="0"><img src="/images/proteomecentral.png"></a></td></tr>
 	</table>	
 
