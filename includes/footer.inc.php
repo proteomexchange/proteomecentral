18282a460d2c714682f04f8c81d03ba000467749
edeutsch <edeutsch@systemsbiology.org>
2023-10-24 20:25:03 -0700

    switch to https

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,13 +5,13 @@
 
 	<div class="footer">
 	<table width="100%"><tr>
-	<td style="vertical-align:top; text-align:left; padding: 10px;">If you have a question or comment about ProteomeXchange, please <a href="http://www.proteomexchange.org/contact">contact us</a>!<BR>
-	<a href="http://www.proteomexchange.org/subscribe/index.html"><img width="70" height="20" src="/images/subscribe_button-small.jpg"><font size="+1" color="#aa0000"> to receive all new ProteomeXchange dataset release announcements!</font></a>
+	<td style="vertical-align:top; text-align:left; padding: 10px;">If you have a question or comment about ProteomeXchange, please <a href="https://www.proteomexchange.org/contact">contact us</a>!<BR>
+	<a href="https://www.proteomexchange.org/subscribe/index.html"><img width="70" height="20" src="/images/subscribe_button-small.jpg"><font size="+1" color="#aa0000"> to receive all new ProteomeXchange dataset release announcements!</font></a>
 	</td>
 	<td style="text-align:right; padding: 10px;">
-	<a class="logolink" target="_blank" href="http://www.ebi.ac.uk/"><img src="/images/ebi_footer_logo.png"></a>&nbsp;&nbsp;&nbsp;&nbsp;
-	<a class="logolink" target="_blank" href="http://www.isbscience.org/"><img src="/images/isb.jpg"></a>&nbsp;&nbsp;&nbsp;&nbsp;
-	<a class="logolink" target="_blank" href="http://ec.europa.eu/research/fp7/index_en.cfm"><img src="/images/capacities.png"></a>
+	<a class="logolink" target="_blank" href="https://www.ebi.ac.uk/"><img src="/images/ebi_footer_logo.png"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+	<a class="logolink" target="_blank" href="https://www.isbscience.org/"><img src="/images/isb.jpg"></a>&nbsp;&nbsp;&nbsp;&nbsp;
+	<a class="logolink" target="_blank" href="https://ec.europa.eu/research/fp7/index_en.cfm"><img src="/images/capacities.png"></a>
 	</td></tr></table>
 	</div>			
 	</body>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -8,7 +8,7 @@
 
 <body bgcolor="#f4f6f8" marginwidth="0" marginheight="0">
 	<table width="100%" height="127" background="/images/topbackground.jpg" style="box-shadow: 0 6px 8px 0 rgba(0,0,0,0.3);">
-	<tr align="center"><td><a href="http://www.proteomexchange.org/" border="0"><img src="/images/proteomexchange.png"></a></td><td><a href="http://proteomecentral.proteomexchange.org/" border="0"><img src="/images/proteomecentral.png"></a></td></tr>
+	<tr align="center"><td><a href="https://www.proteomexchange.org/" border="0"><img src="/images/proteomexchange.png"></a></td><td><a href="https://proteomecentral.proteomexchange.org/" border="0"><img src="/images/proteomecentral.png"></a></td></tr>
 	</table>	
 
 	<!-- Main content area here -->
