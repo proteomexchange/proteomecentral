b4e528a9ed464d661749cd029caadea0f8de3a3f
Luis Mendoza <Luis.Mendoza@systemsbiology.org>
2024-11-13 15:39:09 -0800

    Add general/shared JS utilities

---
+++ javascript/js/utils.js
@@ -0,0 +1,77 @@
+function pc_getRecentItems(type,max=10) {
+    migrate_and_deprecate_me();
+    if (!localStorage["Recent_"+type])
+        return [];
+
+    var recent_items = JSON.parse(localStorage["Recent_"+type]);
+
+    return recent_items.slice(0, max);
+}
+
+function pc_addRecentItem(type,item) {
+    var recent_items = [];
+    if (localStorage["Recent_"+type])
+        recent_items = JSON.parse(localStorage["Recent_"+type]);
+
+    const index = recent_items.indexOf(item);
+    if (index > -1)
+        recent_items.splice(index, 1);
+
+    recent_items.unshift(item);
+
+    localStorage.setItem("Recent_"+type, JSON.stringify(recent_items));
+}
+
+
+// from w3schools (mostly)
+function tpp_dragElement(ele) {
+    ele.style.cursor = "move";
+    var posx1 = 0, posx2 = 0, posy1 = 0, posy2 = 0;
+    if (document.getElementById(ele.id + "header"))
+        document.getElementById(ele.id + "header").onmousedown = dragMouseDown;
+    else
+        ele.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        posx2 = e.clientX;
+        posy2 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        posx1 = posx2 - e.clientX;
+        posy1 = posy2 - e.clientY;
+        posx2 = e.clientX;
+        posy2 = e.clientY;
+        ele.style.top  = (ele.offsetTop  - posy1) + "px";
+        ele.style.left = (ele.offsetLeft - posx1) + "px";
+        ele.style.right = 'initial';
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+// //////////////////////////////////////
+// DEPRECATE THIS soon...
+function migrate_and_deprecate_me() {
+    if (!localStorage.USIcount)
+        return;
+
+    var i = 0;
+    while (i <= Number(localStorage.USIcount)) {
+        var dausi = localStorage.getItem("USI_"+i);
+        if (dausi) {
+            localStorage.removeItem("USI_"+i);
+	    pc_addRecentItem("USI",dausi);
+	}
+        i++;
+    }
+}
